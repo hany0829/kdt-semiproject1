4967d75141f6ccad235ec55cf3643101c6df1aa2
hany0829 <hany8092@naver.com>
2023-04-27 17:04:22 +0900

    base setting

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+urlpatterns = [
+    # path('category/<int:category_pk>/', views.category, name='category'),
+    # path('category/<int:category_pk>/product/<int:product_pk>', views.product, name='product'),
+]
---
+++ reviews/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'reviews'
+urlpatterns = [
+    # path('products/<int:product_pk>/', views.review, name='review'),
+    # path('<int:review_pk>/like/', views.category, name='category'),
+    # path('<<int:review_pk>/delete/', views.delete, name='delete'),
+    # path('<int:review_pk>/update/', views.update, name='update'),
+    # path('<int:review_pk>/comments/', views.comments, name='comments'),
+    # path('<int:review_pk>/comments/<int:comment_pk>', views.comments_like, name='comments_like'),
+    # path('<int:review_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    # path('<int:review_pk>/comments/<int:comment_pk>/update/', views.comments_update, name='comments_update'),
+]
