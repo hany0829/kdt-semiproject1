0ef6f976dab17ee35d3661ff7993e28eadc5270a
armadillo <dltngks54@gmail.com>
2023-04-28 14:46:15 +0900

    product 임시 view 작성

--- products/models.py
+++ products/models.py
@@ -4,12 +4,12 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
 class Product(models.Model):
-    def product_image_path(instance):
-        return f'images/{instance.category.name}/{instance.name}'
+    def product_image_path(instance, filename):
+        png = filename.split('.')[-1]
+        return f'images/{instance.category.name.lower()}/{instance.name}.{png}'
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name     = models.CharField(max_length=50)
     price    = models.PositiveIntegerField()
     origin   = models.CharField(max_length=50, default='JAPAN')
     brand    = models.CharField(max_length=50, default='Karimoku')
     image    = models.ImageField(upload_to=product_image_path)
-    
