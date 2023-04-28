773f6e3cdec78718ec9b74f45c2f5cddda8d9d1e
KDT live <dnjsxo0616@gmail.com>
2023-04-28 14:58:41 +0900

    reviews 임시

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 app_name = 'reviews'
 urlpatterns = [
-    # path('products/<int:product_pk>/', views.review, name='review'),
+    path('category/<int:category_pk>/product/<int:product_pk>/review/create/', views.create_review, name='create_review'),
+    path('category/<int:category_pk>/product/<int:product_pk>/review/<int:review_pk>/', views.review_detail, name='review_detail'),
+    path('category/<int:category_pk>/product/<int:product_pk>/review/<int:review_pk>/delete/', views.delete_review, name='delete_review'),
+    path('category/<int:category_pk>/product/<int:product_pk>/review/<int:review_pk>/update/', views.delete_review, name='delete_review'),
+
     # path('<int:review_pk>/like/', views.category, name='category'),
-    # path('<<int:review_pk>/delete/', views.delete, name='delete'),
-    # path('<int:review_pk>/update/', views.update, name='update'),
+
     # path('<int:review_pk>/comments/', views.comments, name='comments'),
     # path('<int:review_pk>/comments/<int:comment_pk>', views.comments_like, name='comments_like'),
     # path('<int:review_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
