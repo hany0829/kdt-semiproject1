fe76ba14096c8b6099b81eccc35ddc3618cb1899
armadillo <dltngks54@gmail.com>
2023-05-03 00:07:08 +0900

    카테고리 부분 임시완료(필터링x)

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     # 카테고리 별로 상품 나오는
-    path('category/<int:category_pk>/', views.category_products, name='category_products'),
+    path('category/<str:category_name>/', views.category_products, name='category_products'),
     # 상품 디테일
     # path('category/<int:category_pk>/product/<int:product_pk>', views.category_products, name='category_products'),
     path('<int:product_pk>/', views.product_detail, name='product_detail'),
