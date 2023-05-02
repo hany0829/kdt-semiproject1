f308f3262b8ff42728c00b4f31591804f8dcb715
armadillo <dltngks54@gmail.com>
2023-05-02 15:32:11 +0900

    인덱스 페이지 백엔드/프론트 결합 테스트

--- pjt/urls.py
+++ pjt/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from products import views
 
+# 이미지를 위해
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +28,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('reviews/', include('reviews.urls')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
