d82d843fe68aa713a33cf22de2af2a13e701b9a1
armadillo <dltngks54@gmail.com>
2023-05-03 10:47:43 +0900

    덤프데이터 생성

--- reviews/models.py
+++ reviews/models.py
@@ -4,14 +4,14 @@ from products.models import Product
 
 # Create your models here.
 class Review(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     # 리뷰 좋아요
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
     # 이미지
-    image = models.ImageField(blank=True)
-    title = models.CharField(max_length=20)
-    content = models.TextField()
+    image      = models.ImageField(blank=True)
+    title      = models.CharField(max_length=20)
+    content    = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     # 별점 구현
     rating = models.PositiveSmallIntegerField(default=0)
