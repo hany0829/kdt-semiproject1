00877818de4661fa2d3ad0a237fa0c62b511c9b3
armadillo <dltngks54@gmail.com>
2023-05-05 00:12:03 +0900

    검색기능 및 디버깅

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,10 @@ app_name = 'products'
 urlpatterns = [
     # 카테고리 별로 상품 나오는
     path('category/<str:category_name>/', views.category_products, name='category_products'),
-    # 상품 디테일
-    # path('category/<int:category_pk>/product/<int:product_pk>', views.category_products, name='category_products'),
+    # 디테일
     path('<int:product_pk>/', views.product_detail, name='product_detail'),
+    # 검색(쿼리스트링으로 받을꺼임)
+    path('search/', views.search, name='search'),
+    # 인덱스
     path('', views.index, name='index'),
 ]
